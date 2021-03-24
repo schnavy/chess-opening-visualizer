afc293be10c0095f6c09a08c2f8fc6ab69a9d56c
David <50714711+schnavy@users.noreply.github.com>
2021-03-24 17:18:15 +0100

    refactor und multiple boards

--- board.js
+++ board.js
@@ -1,36 +1,72 @@
-const board = document.querySelector("#board")
-const numberdiv = document.querySelector("#labeling .numberwrapper")
-const letterdiv = document.querySelector("#labeling .letterwrapper")
-const fields = [];
-const letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
-const label = {
-    letters: [],
-    numbers: []
-}
-counter = 0;
+class Board {
+    constructor(size) {
+        this.size = size;
+        this.counter = 0;
+        this.fields = [];
+        this.letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        fields[counter] = document.createElement("div");
-        fields[counter].classList.add("field")
-        fields[counter].id = "row" + i + "col" + j;
-        if ((i + j) % 2 == 0) {
-            fields[counter].classList.add("odd")
+
+        this.boardwrapper = generateDiv(container, "boardwrapper", "board" + boardscounter);
+        this.boardwrapper.style.width = size;
+        this.poshelper = generateDiv(this.boardwrapper, "poshelper");
+        this.board = generateDiv(this.poshelper, "board");
+        this.labeling = generateDiv(this.poshelper, "labeling");
+        this.numberwrapper = generateDiv(this.labeling, "numberwrapper");
+        this.letterwrapper = generateDiv(this.labeling, "letterwrapper");
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                this.fields[this.counter] = document.createElement("div");
+                this.fields[this.counter].classList.add("field")
+                this.fields[this.counter].id = "row" + i + "col" + j;
+                if ((i + j) % 2 == 0) {
+                    this.fields[this.counter].classList.add("odd")
+                }
+                this.board.appendChild(this.fields[this.counter])
+                this.counter += 1
+
+            }
         }
-        board.appendChild(fields[counter])
-        counter += 1
+        for (let i = 0; i < 8; i++) {
+            let l;
+            l = document.createElement("div");
+            l.classList.add("letter")
+            l.textContent = this.letters[i];
+            this.letterwrapper.appendChild(l)
+        }
+        for (let i = 0; i < 8; i++) {
+            let n;
+            n = document.createElement("div");
+            n.classList.add("number")
+            n.textContent = 8 - i;
+            this.numberwrapper.appendChild(n)
+        }
+
+        boardscounter++
+
 
     }
-}
-for (let i = 0; i < 8; i++) {
-    label.letters[i] = document.createElement("div");
-    label.letters[i].classList.add("letter")
-    label.letters[i].textContent = letters[i];
-    letterdiv.appendChild(label.letters[i])
-}
-for (let i = 0; i < 8; i++) {
-    label.numbers[i] = document.createElement("div");
-    label.numbers[i].classList.add("number")
-    label.numbers[i].textContent = 8 - i;
-    numberdiv.appendChild(label.numbers[i])
+
+    updatePosition(array) {
+        this.fields.forEach(elem => {
+            elem.innerHTML = ""
+
+        })
+        for (const i in this.fields) {
+            let piece = array[i];
+            if (piece != " ") {
+                if (piece == piece.toUpperCase()) {
+                    this.fields[i].innerHTML = "<div id=w" + piece.toLowerCase() + " class ='piece white'></div>"
+                } else {
+
+                    this.fields[i].innerHTML = "<div id=b" + piece.toLowerCase() + " class ='piece'></div>"
+                }
+            }
+        }
+
+    }
+    resetToStartPos() {
+        this.updatePosition(startArray)
+        document.querySelector("form").reset();
+    }
 }
--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const resetBtn = document.querySelector("#reset")
+let boardscounter = 0;
+const container = document.querySelector("#container")
+const resetBtn = document.querySelector("#reset");
+const newBoardBtn = document.querySelector(".newboardbtn")
+const removeBoardBtn = document.querySelector(".removeboardbtn")
+
 const startingPos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 const convertedStartingPos = startingPos.replaceAll("8", "        ").replaceAll("/", "")
 const startArray = Array.from(convertedStartingPos)
-
-updatePosition(startArray)
+let boards = []
 
 resetBtn.addEventListener("click", (e) => {
     resetToStartPos()
 })
 
-
+boards[0] = new Board()
+boards[0].resetToStartPos()
 
 function handleButton(fen) {
     let arr = convertFEN(fen)
@@ -17,33 +22,9 @@ function handleButton(fen) {
         alert("bitte das FENN Format benutzen")
         return
     }
-    updatePosition(arr)
+    board0.updatePosition(arr)
 }
 
-function updatePosition(array) {
-    fields.forEach(elem => {
-        elem.innerHTML = ""
-
-    })
-    for (const i in fields) {
-        let piece = array[i];
-        if (piece != " ") {
-            if (piece == piece.toUpperCase()) {
-                fields[i].innerHTML = "<div id=w" + piece.toLowerCase() + " class ='piece white'></div>"
-            } else {
-
-                fields[i].innerHTML = "<div id=b" + piece.toLowerCase() + " class ='piece'></div>"
-            }
-        }
-    }
-
-}
-
-function resetToStartPos() {
-    updatePosition(startArray)
-    document.querySelector("form").reset();
-
-}
 // Image generator :  https://www.npmjs.com/package/chess-image-generator
 
 
@@ -53,4 +34,41 @@ function convertFEN(str) {
     }
     const startArray = Array.from(str.replaceAll("8", "        ").replaceAll("7", "       ").replaceAll("6", "       ").replaceAll("5", "     ").replaceAll("4", "    ").replaceAll("3", "   ").replaceAll("2", "  ").replaceAll("1", " ").replaceAll("/", ""));
     return startArray
+}
+
+
+// poswrapper.addEventListener("mouseover", (e) => {
+//     if (window.innerWidth > 768) {
+//         let mx = -map(e.x, 0, window.innerWidth, -100, 100);
+//         let my = -map(e.y, 0, window.innerHeight, -100, 100);
+//         poswrapper.setAttribute('style', 'transform:translate(' + mx + 'px,' + my + 'px)');
+//     } else {
+//         poswrapper.setAttribute('style', 'transform:translate(' + 0 + 'px,' + 0 + 'px)');
+//     }
+// })
+
+
+newBoardBtn.addEventListener("click", (e) => {
+    boards[boards.length] = new Board()
+    boards[boards.length - 1].resetToStartPos()
+
+})
+
+removeBoardBtn.addEventListener("click", (e) => {
+    if (boards.length > 1) {
+        boards[boards.length - 1].boardwrapper.parentNode.removeChild(boards[boards.length - 1].boardwrapper)
+        boards.pop();
+    }
+})
+
+function map(value, x1, y1, x2, y2) {
+    return ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+}
+
+function generateDiv(parent, klasse, id) {
+    let temp = document.createElement("div");
+    if (klasse) temp.classList.add(klasse);
+    if (id) temp.id = id;
+    parent.appendChild(temp)
+    return temp;
 }
